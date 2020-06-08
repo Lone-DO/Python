727b5451b638b3da18f1d8738f1dc415164f2b64
Lone-DO <Lone-DO@users.noreply.github.com>
2020-06-08 16:18:27 -0400

    Initializing List Command

--- main.py
+++ main.py
@@ -12,8 +12,18 @@ while True:
     Enjoy
     """)
     userInput = input("\t")
+    args = userInput.lower().split(" ")
     if (userInput.lower() in Leave): break
-    if (userInput.lower() == "load"): 
+    if (args[0] == "load"): 
         # reddit.fetch()
         Display(reddit.data)
+    if (args[0] == "list"):
+        if len(args) > 1:
+            index = int(args[1])
+            if index < len(Display.list):
+                print(Display.list[index])
+            elif index >= len(Display.list):
+                print("Page Unavailable, Please try again")
+        else:
+            print(Display.list)
 print("Goodbye")
